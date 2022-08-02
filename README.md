084cb284993be7f4678850d8c02c871144fc1314
Ashlynn Landry <ashclandry@gmail.com>
2022-08-01 22:02:18 -0400

    html complete, started on game.js, need to add comments and more

---
+++ README.md
@@ -0,0 +1,46 @@
+# Web APIs Challenge: JavaScript Code Quiz
+
+DISCLAIMER: I was not able to finish this project in the alloted time so it does not function as it should. The only thing I was able to build out was the HTML and CSS
+
+## About the Repository
+The purpose of this repository was to create JavaScript Code Quiz that features dynamically updated HTML CSS powered by JavaScript code. When you deploy the application you will see that it has a clean, polished, and responsive user interface.
+
+## Acceptance Criteria
+For this project, I was given a set of critical requirements to build a timed quiz on JavaScript fundamentals so that I can gauge my progress compared to my peers. They are as follows:
+
+![Screenshot of acceptance criteria](./assets/images/Screen%20Shot%202022-07-25%20at%205.23.02%20PM.png)
+
+## Additional instruction to complete the final challenge included:
+* Application deploys at live URL with no loading errors. 
+* Application user experience is intuitive, easy to navigate, and the use interface style is clean and polished.
+* Repository as a unique name, has proper file structure, naming conventions, proper indentation, quality comments, and a quality README file with description, screenshot of the final project, and a link to the live deployed application.
+* Application resembles the following mock-up functionality: 
+
+![gif of mock-up](./assets/images/04-web-apis-homework-demo.gif)
+
+## Wireframe Mock-Up of my quiz:
+![Wireframe](./assets/images/JS%20Quiz%20Wireframe.png)
+
+## Description and Credits 
+
+To create this application, I sought help from multiple resources, which include:
+
+* YouTube: https://www.youtube.com/watch?v=RswgVWKJRLM ; https://www.youtube.com/watch?v=riDzcEQbX6k 
+* Quiz questions from w3schools: https://www.w3schools.com/js/js_quiz.asp 
+* mdn web docs: https://developer.mozilla.org/en-US/docs/Web/API/setTimeout ;
+* Google fonts
+* I referenced previous activities from "03-JavaScript", "04-Web-APIs" and "05 Third-Party-APIs"
+* I also sought help from my peers. 
+* I used the following link to help me with the layout for this README file:
+https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+
+## Final submission links:
+
+* GitHub Repository - 
+* GitHub Deployed Page - 
+
+
+## This is what my final Code Quiz looks like:
+![JavaScript Code Quiz](./assets/images/JavaScript%20Quiz%20(1).mp4)
+
+
---
+++ game.js
@@ -0,0 +1,115 @@
+var questionContainerElement = document.getElementById('question-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-buttons')
+var timerEl = document.getElementById('countdown')
+
+var shuffledQuestions, currentQuestionIndex
+
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+
+
+var questions = [
+  {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answers: [
+      { choice1: '<javascript>', correct: false },
+      { choice2: '<scripting>', correct: false },
+      { choice3: '<js>', correct: false },
+      { choice4: '<script>', correct: true },
+    ]
+  },
+
+  {
+    question: 'Where is the correct place to insert a JavaScript?',
+    answers: [
+      { choice1: 'The <body> section', correct: false },
+      { choice2: 'The <head> section', correct: false },
+      { choice3: 'Both the <head> section and the <body> section are correct', correct: true },
+      { choice4: 'Inside the JavaScript file', correct: false },
+    ]
+  },
+
+  {
+    question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+    answers: [
+      { choice1: '<script ="xxx.js">', correct: false },
+      { choice2: '<script href=”xxx.js”>', correct: false },
+      { choice3: '<script src=”xxx.js”>', correct: true },
+      { choice4: '<script name=”xxx.js”>', correct: false },
+    ]
+  },
+
+  {
+    question: 'How do you write "Hello World" in an alert box?',
+    answers: [
+      { choice1: 'alert(“Hello Word”);', correct: true },
+      { choice2: 'msg(“Hello World”);', correct: false },
+      { choice3: 'msgBox(“Hello World”);', correct: false },
+      { choice4: 'alertBox(“Hello World”);', correct: false },
+    ]
+  },
+
+  {
+    question: 'How do you create a function in JavaScript?',
+    answers: [
+      { choice1: 'function myFunction()', correct: true },
+      { choice2: 'function = myFunction()', correct: false },
+      { choice3: 'function:myFunction()', correct: false },
+      { choice4: 'create.function()', correct: false },
+    ]
+  },
+
+  {
+    question: 'How do you call a function named "myFunction"?',
+    answers: [
+      { choice1: 'call myFunction()', correct: false },
+      { choice2: 'myFunction()', correct: true },
+      { choice3: 'call function myFunction()', correct: false },
+      { choice4: 'call(myFunction)', correct: false },
+    ]
+  },
+
+  {
+    question: 'How to write an IF statement in JavaScript?',
+    answers: [
+      { choice1: 'if i = 5', correct: false },
+      { choice2: 'if i == 5 then', correct: false },
+      { choice3: 'if (i == 5)', correct: true },
+      { choice4: 'if i = 5 then ', correct: false },
+    ]
+  },
+
+  {
+    question: 'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    answers: [
+      { choice1: 'if i =! 5 then ', correct: false },
+      { choice2: 'if (i <> 5)', correct: false },
+      { choice3: 'if i <> 5', correct: false },
+      { choice4: 'if ( i !=5)', correct: true },
+    ]
+  },
+
+  {
+    question: 'How does a WHILE loop start?',
+    answers: [
+      { choice1: 'while (i <= 1-; i++)', correct: false },
+      { choice2: 'while i = 1 to 10', correct: false },
+      { choice3: 'while (i <= 10)', correct: true },
+      { choice4: 'while (i) = 1 - 10', correct: false },
+    ]
+  },
+
+  {
+    question: 'How does a FOR loop start?',
+    answers: [
+      { choice1: 'for (i = 0; i <= 5; i++)', correct: true },
+      { choice2: 'for i = 1 to 5 ', correct: false },
+      { choice3: 'for (i = 0; i <= 5)', correct: false },
+      { choice4: 'for (i <= 5; i++)', correct: false },
+    ]
+  }
+]
